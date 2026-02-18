19ef4d1791a7e88076ebaca2d7b69978d179cdcb
Pardeepan10 <a.p.muthupardeepan@gmail.com>
2026-02-18 21:51:36 +0530

    Add files via upload

--- src/Library.java
+++ src/Library.java
@@ -1,1 +1,15 @@
+class Library{
+    int Bookid;
+    String title;
+    String Authorname;
+    String genre;
+    int bookQauntity;
 
+    Library(int bookid,String Title,String authorname,String genre,int bookQauntity){
+         this.Bookid=bookid;
+        this.title=Title;
+         this.genre=genre;
+         this.Authorname=authorname;
+         this.bookQauntity=bookQauntity;
+    }
+}
