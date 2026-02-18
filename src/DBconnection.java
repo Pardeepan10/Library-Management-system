ce0c17d0d85c24e0aa8c1cb5202930f047732f02
Pardeepan10 <a.p.muthupardeepan@gmail.com>
2026-02-18 21:52:30 +0530

    Add files via upload

--- src/DBconnection.java
+++ src/DBconnection.java
@@ -1,1 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBconnection {
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/librarydb",
+                "root",
+                "Pardee@"
+        );
+    }
+}
